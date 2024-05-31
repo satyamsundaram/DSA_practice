a7e57a5882b92c37a459a95946b9fb513c413057
Satyam Sundaram <satyamsundaram01@gmail.com>
2024-05-31 18:35:55 +0530

    add new problem

--- strivers_a2z_sheet/BT/postorder-traversal.cpp
+++ strivers_a2z_sheet/BT/postorder-traversal.cpp
@@ -13,3 +13,27 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+class Solution
+{
+public:
+    // simple recursion
+    // TC: O(N)
+    // SC: O(N)
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        vector<int> v;
+        postorderHelper(root, v);
+        return v;
+    }
+
+    void postorderHelper(TreeNode *root, vector<int> &v)
+    {
+        if (!root)
+            return;
+        if (root->left)
+            postorderHelper(root->left, v);
+        if (root->right)
+            postorderHelper(root->right, v);
+        v.push_back(root->val);
+    }
+};
