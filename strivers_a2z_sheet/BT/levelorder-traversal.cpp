53d1cd190c3ff6d0e0408726fb622851b372dab4
Satyam Sundaram <satyamsundaram01@gmail.com>
2024-06-18 16:46:59 +0530

    add new problem

--- strivers_a2z_sheet/BT/levelorder-traversal.cpp
+++ strivers_a2z_sheet/BT/levelorder-traversal.cpp
@@ -13,3 +13,38 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+class Solution
+{
+public:
+    // using a queue
+    // TC: O(N) where N is the no. of elements in the tree
+    // SC: O(N)
+    vector<vector<int>> levelOrder(TreeNode *root)
+    {
+        if (!root)
+            return {};
+
+        vector<vector<int>> res;
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            vector<int> temp;
+            int sz = q.size();
+            for (int i = 0; i < sz; i++)
+            {
+                TreeNode *qf = q.front();
+                temp.push_back(qf->val);
+                if (qf->left)
+                    q.push(qf->left);
+                if (qf->right)
+                    q.push(qf->right);
+                q.pop();
+            }
+            res.push_back(temp);
+        }
+
+        return res;
+    }
+};
