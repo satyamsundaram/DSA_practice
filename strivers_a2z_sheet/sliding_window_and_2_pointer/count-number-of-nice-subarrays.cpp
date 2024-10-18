99a16a3d1438b0ae9efa97c57bdd342642f6ab2f
Satyam Sundaram <satyamsundaram01@gmail.com>
2024-10-18 09:06:59 +0530

    add new problems

--- strivers_a2z_sheet/sliding_window_and_2_pointer/count-number-of-nice-subarrays.cpp
+++ strivers_a2z_sheet/sliding_window_and_2_pointer/count-number-of-nice-subarrays.cpp
@@ -3,3 +3,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    // // naive: for each possible subarray check if it has k odd numbers O(n^2) => TLE
+    // int numberOfSubarrays(vector<int>& nums, int k) {
+    //     int n = nums.size(), niceCnt = 0;
+    //     for(int i=0; i<n; i++) {
+    //         int oddCnt = 0;
+    //         for(int j=i; j<n; j++) {
+    //             if(nums[j]%2) oddCnt++;
+    //             if(oddCnt==k) niceCnt++;
+    //         }
+    //     }
+
+    //     return niceCnt;
+    // }
+
+    // --------------------------------------- //
+
+    // optimized: number of subarrays with at most k odd elements - number of subarrays with at most k-1 odd elements which can be handled with sliding window; if count violated, shrink window from left and update count
+    // TC: O(n)
+    // SC: O(1)
+    int numberOfSubarrays(vector<int> &nums, int k)
+    {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    int atMost(vector<int> &nums, int k)
+    {
+        int n = nums.size(), left = 0, right = 0, cnt = 0, oddCnt = 0;
+        while (right < n)
+        {
+            if (nums[right] % 2)
+                oddCnt++;
+            while (oddCnt > k)
+            {
+                if (nums[left] % 2)
+                    oddCnt--;
+                left++;
+            }
+            cnt += (right - left + 1);
+            right++;
+        }
+
+        return cnt;
+    }
+};
--- strivers_a2z_sheet/sliding_window_and_2_pointer/maximum-points-you-can-obtain-from-cards.cpp
+++ strivers_a2z_sheet/sliding_window_and_2_pointer/maximum-points-you-can-obtain-from-cards.cpp
@@ -3,3 +3,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    // // naive: try all the combinations eg: if we need to pick k cards, pick 0 from left, k from right, next pick 1 from left, k-1 from right, 2 from left, k-2 from right until k from left, 0 from right
+    // // TC: O(n*k) => TLE
+    // // SC: O(1)
+    // int maxScore(vector<int>& cardPoints, int k) {
+    //     int fromLeft = 0, fromRight = k, res = INT_MIN, n = cardPoints.size();
+    //     while(fromLeft <= k) {
+    //         int sum = 0;
+    //         for(int i=0; i<fromLeft; i++)
+    //             sum += cardPoints[i];
+    //         for(int i=n-1; i>=n-fromRight; i--)
+    //             sum += cardPoints[i];
+    //         res = max(res, sum);
+    //         fromLeft++;
+    //         fromRight--;
+    //     }
+
+    //     return res;
+    // }
+
+    // ---------------------------------------------------------- //
+
+    // optimized: if total_pts is sum of all cards, we need to remove subarray of length n-k with minimum sum and use the sum of the rest of the cards as our answer.
+    // TC: O(n)
+    // SC: O(n)
+    int maxScore(vector<int> &cardPoints, int k)
+    {
+        int totalPts = 0, n = cardPoints.size();
+        for (int point : cardPoints)
+            totalPts += point;
+
+        if (n == k)
+            return totalPts;
+
+        int left = 0, right = 0, sum = 0, minSum = INT_MAX;
+        while (right < n - k)
+            sum += cardPoints[right++];
+        minSum = min(minSum, sum);
+
+        while (right < n)
+        {
+            sum += cardPoints[right++];
+            sum -= cardPoints[left++];
+            minSum = min(minSum, sum);
+        }
+
+        return totalPts - minSum;
+    }
+};
