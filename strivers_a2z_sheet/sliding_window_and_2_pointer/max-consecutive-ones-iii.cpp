e81ff697d85a95c9b2d0f0ab44a46e34e1f43305
Satyam Sundaram <satyamsundaram01@gmail.com>
2024-10-20 13:36:24 +0530

    add problems

--- strivers_a2z_sheet/sliding_window_and_2_pointer/max-consecutive-ones-iii.cpp
+++ strivers_a2z_sheet/sliding_window_and_2_pointer/max-consecutive-ones-iii.cpp
@@ -3,3 +3,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    // // naive: O(n^2) => run 2 nested loops and the inner loop should run till we encounter a zero and k == 0
+    // int longestOnes(vector<int>& nums, int k) {
+    //     int res = 0, n = nums.size();
+    //     for(int i=0; i<n; i++) {
+    //         int temp = k, cnt = 0;
+    //         for(int j=i; j<n; j++) {
+    //             if(nums[j]) cnt++;
+    //             else if(temp) {
+    //                 cnt++;
+    //                 temp--;
+    //             } else break;
+
+    //             res = max(res, cnt);
+    //         }
+    //     }
+
+    //     return res;
+    // }
+
+    // -------------------------------------------- //
+
+    // optimized: maintain a valid window size where k>=0 using sliding window in order to not compute same values again for max length
+    // TC: O(n)
+    int longestOnes(vector<int> &nums, int k)
+    {
+        int left = 0, right = 0, res = 0, n = nums.size();
+        while (right < n)
+        {
+            if (!nums[right])
+            {
+                if (k)
+                    k--;
+                else
+                    while (nums[left++])
+                        ;
+            }
+
+            res = max(res, right - left + 1);
+            right++;
+        }
+
+        return res;
+    }
+};
--- strivers_a2z_sheet/sliding_window_and_2_pointer/number-of-substrings-containing-all-three-characters.cpp
+++ strivers_a2z_sheet/sliding_window_and_2_pointer/number-of-substrings-containing-all-three-characters.cpp
@@ -3,3 +3,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    // // naive: O(n^2) => 2 nested for loops, inside nested loop check if we've got a,b & c, and keep counting substrings
+    // int numberOfSubstrings(string s) {
+    //     int cnt = 0, n = s.size();
+    //     for(int i=0; i<n; i++) {
+    //         int a=0, b=0, c=0;
+    //         for(int j=i; j<n; j++) {
+    //             if(s[j]=='a') a=1;
+    //             else if(s[j]=='b') b=1;
+    //             else c=1;
+    //             if(a && b && c) cnt++;
+    //         }
+    //     }
+
+    //     return cnt;
+    // }
+
+    // ------------------------------------ //
+
+    // optimized: using sliding window to maintain a valid window aka with a,b & c && add n-j to cnt
+    // TC: O(n)
+    int numberOfSubstrings(string s)
+    {
+        int cnt = 0, left = 0, right = 0, n = s.size(), a = 0, b = 0, c = 0;
+        while (right < n)
+        {
+            if (s[right] == 'a')
+                a++;
+            else if (s[right] == 'b')
+                b++;
+            else
+                c++;
+
+            while (a && b && c)
+            {
+                cnt += (n - right);
+                if (s[left] == 'a')
+                    a--;
+                else if (s[left] == 'b')
+                    b--;
+                else
+                    c--;
+                left++;
+            }
+
+            right++;
+        }
+
+        return cnt;
+    }
+};
