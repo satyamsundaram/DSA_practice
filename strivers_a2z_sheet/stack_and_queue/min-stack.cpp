541fdeac0faf30d16e4b7545ad382d068035c64e
Satyam Sundaram <satyamsundaram01@gmail.com>
2024-10-21 11:09:47 +0530

    add new problems

--- strivers_a2z_sheet/stack_and_queue/min-stack.cpp
+++ strivers_a2z_sheet/stack_and_queue/min-stack.cpp
@@ -3,3 +3,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// // O(logn)
+// class MinStack {
+// private:
+//     set<int> s;
+//     unordered_map<int, int> um;
+//     stack<int> st;
+
+// public:
+//     MinStack() {}
+
+//     void push(int val) {
+//         st.push(val);
+//         um[val]++;
+//         s.insert(val); // O(logn)
+//     }
+
+//     void pop() {
+//         int ele = st.top();
+//         st.pop();
+//         um[ele]--;
+//         if(!um[ele]) s.erase(ele); // O(logn)
+//     }
+
+//     int top() {
+//         return st.top();
+//     }
+
+//     int getMin() {
+//         set<int>::iterator itr = s.begin();
+//         return *itr;
+//     }
+// };
+
+// ------------------------------------------ //
+
+// O(1): each ele of the stack also contains the min including it and the elements below it (they're kept in a pair)
+class MinStack
+{
+private:
+    stack<pair<int, int>> st;
+
+public:
+    MinStack() {}
+
+    void push(int val)
+    {
+        if (st.empty())
+            st.push({val, val});
+        else
+            st.push({val, min(val, st.top().second)});
+    }
+
+    void pop()
+    {
+        st.pop();
+    }
+
+    int top()
+    {
+        return st.top().first;
+    }
+
+    int getMin()
+    {
+        return st.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
--- strivers_a2z_sheet/stack_and_queue/valid-parentheses.cpp
+++ strivers_a2z_sheet/stack_and_queue/valid-parentheses.cpp
@@ -3,3 +3,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    // SC & TC: O(n) => using stack, we insert all opening brackets in order, when we encounter a closing bracket, we check top of stack to check if the closing bracket is correct in order, if yes, we pop and move ahead, else return false
+    bool isValid(string s)
+    {
+        stack<int> st;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                st.push(s[i]);
+            else if (st.empty())
+                return false;
+            else
+            {
+                if (s[i] == ')' && st.top() != '(')
+                    return false;
+                else if (s[i] == '}' && st.top() != '{')
+                    return false;
+                else if (s[i] == ']' && st.top() != '[')
+                    return false;
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
