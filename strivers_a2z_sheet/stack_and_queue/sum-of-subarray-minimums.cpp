70478651086e15eccf30ee80c7e6326958affa0c
Satyam Sundaram <satyamsundaram01@gmail.com>
2024-10-23 15:41:43 +0530

    add new problems

--- strivers_a2z_sheet/stack_and_queue/sum-of-subarray-minimums.cpp
+++ strivers_a2z_sheet/stack_and_queue/sum-of-subarray-minimums.cpp
@@ -3,3 +3,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    // // naive: two nested loops
+    // // TC: O(n^2) => TLE
+    // // SC: O(1)
+    // int sumSubarrayMins(vector<int>& nums) {
+    //     int mod = 1e9+7, res = 0, n = nums.size();
+    //     for(int i=0; i<n; i++) {
+    //         int mn = nums[i];
+    //         for(int j=i; j<n; j++) {
+    //             mn = min(mn, nums[j]);
+    //             res += mn;
+    //             res %= mod;
+    //         }
+    //     }
+
+    //     return res%mod;
+    // }
+
+    // -------------------------------------- //
+
+    // optimized: for each index we need to know how many subarrays it is a part of where it is the minimum, we can find this by counting for each index, how many numbers to its left are >= it (the rightmost ele will be minimum if same numbers are repeated) and how many numbers to its right are > (strictly) it. then each ele's contribution to sum = arr[i] * left[i] * right[i]; we can get this count using stack => why stack? for each ele we need to keep track of all ele's before it which were >= or > than it depending on our use case, so we need it to keep track
+    // TC: O(n)
+    // SC: O(n)
+    int sumSubarrayMins(vector<int>& nums) {
+        int mod = 1e9+7, n = nums.size();
+        long long res = 0;
+        vector<int> left(n), right(n);
+        stack<pair<int, int>> s1, s2; // {ele, count}
+
+        // left
+        for(int i=0; i<n; i++) {
+            int cnt = 1;
+            while(!s1.empty() && s1.top().first >= nums[i]) {
+                cnt += s1.top().second;
+                s1.pop();
+            }
+
+            left[i]=cnt;
+            s1.push({nums[i], cnt});
+        }
+
+        // right
+        for(int i=n-1; i>=0; i--) {
+            int cnt = 1;
+            while(!s2.empty() && s2.top().first > nums[i]) {
+                cnt += s2.top().second;
+                s2.pop();
+            }
+
+            right[i]=cnt;
+            s2.push({nums[i], cnt});
+        }
+
+        for(int i=0; i<n; i++) {
+            res = (res + (long long)nums[i]*left[i]*right[i]) % mod;
+        }
+
+        return res;
+    }
+};
--- strivers_a2z_sheet/stack_and_queue/trapping-rain-water.cpp
+++ strivers_a2z_sheet/stack_and_queue/trapping-rain-water.cpp
@@ -3,3 +3,85 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    // // naive: O(n^2) => for every bar, find max bar length to its left and right (strictly greater), the min of the two - current bar length is the amount of water stored on top of that bar; sum for all bars will give our answer
+    // int trap(vector<int>& bars) {
+    //     int n = bars.size(), res = 0;
+
+    //     for(int i=0; i<n; i++) {
+    //         int maxl = 0, maxr = 0;
+
+    //         for(int j=0; j<i; j++) {
+    //             if(bars[j] > bars[i]) maxl = max(maxl, bars[j]);
+    //         }
+
+    //         if(!maxl) continue;
+
+    //         for(int j=i+1; j<n; j++) {
+    //             if(bars[j] > bars[i]) maxr = max(maxr, bars[j]);
+    //         }
+
+    //         if(!maxr) continue;
+
+    //         res += (min(maxl, maxr) - bars[i]);
+    //     }
+
+    //     return res;
+    // }
+
+    // // ---------------------------------------------- //
+
+    // // optimized: instead of checking entire left and right array for max height, we pre-compute and store for each index in order to only check once
+    // // TC: O(n)
+    // // SC: O(n)
+    // int trap(vector<int>& bars) {
+    //     int n = bars.size(), res = 0;
+    //     vector<int> left(n, 0), right(n, 0);
+
+    //     for(int i=1; i<n; i++)
+    //         left[i] = max(left[i-1], bars[i-1]);
+
+    //     for(int i=n-2; i>=0; i--)
+    //         right[i] = max(right[i+1], bars[i+1]);
+
+    //     for(int i=1; i<n-1; i++) {
+    //         int water = min(left[i], right[i]) - bars[i];
+    //         if(water > 0) res += water;
+    //     }
+
+    //     return res;
+    // }
+
+    // ---------------------------------------------- //
+
+    // space-optimized: instead of saving max height on left and right for each ele in 2 arrays, we use 2 pointers to keep track of max height on left and right discovered so far
+    // TC: O(n)
+    // SC: O(1)
+    int trap(vector<int> &bars)
+    {
+        int n = bars.size(), res = 0, maxl = 0, maxr = 0, l = 0, r = n - 1;
+        while (l <= r)
+        {
+            if (bars[l] < bars[r])
+            {
+                if (bars[l] >= maxl)
+                    maxl = bars[l];
+                else
+                    res += (maxl - bars[l]);
+                l++;
+            }
+            else
+            {
+                if (bars[r] >= maxr)
+                    maxr = bars[r];
+                else
+                    res += (maxr - bars[r]);
+                r--;
+            }
+        }
+
+        return res;
+    }
+};
